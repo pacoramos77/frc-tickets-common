2da6eb72f86f2ed25996a4afe698dadd56b88755
Francisco Ramos <franramos@outlook.com>
2021-10-13 11:57:38 +0200

    Updates

--- src/index.ts
+++ src/index.ts
@@ -18,5 +18,6 @@ export * from "./events/ticket-updated-event";
 export * from "./events/order-created-event";
 export * from "./events/order-cancelled-event";
 export * from "./events/expiration-complete-event";
+export * from "./events/payment-created-event";
 
 export * from "./events/types/order-status";
